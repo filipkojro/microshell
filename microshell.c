2909d12b843041ce98d2b0d4fe6b0c10c1d3f498
Filip Kojro <filip@Filips-MacBook-Air.local>
2024-11-10 16:40:32 +0100

    running commands

--- microshell.c
+++ microshell.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
@@ -7,29 +8,50 @@
 #define true 1
 #define false 0
 
-int cp(argc, argv, envp){
-    printf("cp\n");
-    return 0;
+char* concat(const char *s1, const char *s2) {
+    const size_t len1 = strlen(s1);
+    const size_t len2 = strlen(s2);
+    char *result = malloc(len1 + len2 + 1); // +1 for the null-terminator
+    memcpy(result, s1, len1);
+    memcpy(result + len1, s2, len2 + 1); // +1 to copy the null-terminator
+    return result;
 }
 
 int main(){
     char command[64];
     char arguments[128];
 
-    printf(" zacheta $ ");
-    pridtf("%d\n", scanf("%s%[^\n]", command, arguments));
+    char cwd[4096];
 
-    printf("%s\n%s\n", command, arguments);
+    if (getcwd(cwd, sizeof(cwd)) == NULL) {
+        printf("getcwd() error?\n");
+        return 1;
+    } 
+    // printf("%s\n\n", cwd);
+    
+
+    printf("%s $ ", cwd);
+    scanf("%s%[^\n]", command, arguments);
+
+    printf("command:%s\narguments:%s\n", command, arguments);
 
     if (fork() != 0){
-        printf("rodzic?\n");
+        // printf("rodzic?\n");
+
         int status;
         waitpid(-1, &status, 0);
+
         printf("returned with status %d\n", status);
     }
     else {
-        printf("dziecko\n");
-        // execve(cp, "22", 0);
+        char* path_to_command = concat("./commands/", command);
+        char *args[] = {NULL};
+
+        execv(path_to_command, args);
+
+        printf("nie bylo takiej komendy\n");
+
+        free(path_to_command);
         return 0;
     }
     return 0;

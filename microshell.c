cf1028932ff3817df55ac5929e07473c950b29a4
Filip Kojro <filip@Filips-MacBook-Air.local>
2024-11-10 14:28:58 +0100

    good start

--- microshell.c
+++ microshell.c
@@ -1,8 +1,36 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <string.h>
 
-int main()
-{
-    printf("hello world");
-    
+#define true 1
+#define false 0
+
+int cp(argc, argv, envp){
+    printf("cp\n");
+    return 0;
+}
+
+int main(){
+    char command[64];
+    char arguments[128];
+
+    printf(" zacheta $ ");
+    pridtf("%d\n", scanf("%s%[^\n]", command, arguments));
+
+    printf("%s\n%s\n", command, arguments);
+
+    if (fork() != 0){
+        printf("rodzic?\n");
+        int status;
+        waitpid(-1, &status, 0);
+        printf("returned with status %d\n", status);
+    }
+    else {
+        printf("dziecko\n");
+        // execve(cp, "22", 0);
+        return 0;
+    }
     return 0;
 }

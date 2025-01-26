d4552dd0a3112299763d9955d19ac225fa97f9ee
Filip <filip.kojro@outlook.com>
2025-01-26 22:23:40 +0100

    arguments have weird behaviour; started doing one way list

---
+++ commands/myhelp.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    char* help_propmt = "Microshell\n"
+                        "Autor: Filip Kojro\n"
+                        "Wersja: ???:latest\n"
+                        "Dostepne komendy:\n"
+                        " - help\n"
+                        " - exit\n"
+                        " - myls\n";
+    printf("%s\n", help_propmt);
+    return 0;
+}

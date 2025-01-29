1849af723387c9cd1c56fff2a1842913590cf762
Filip <filip.kojro@outlook.com>
2025-01-29 11:08:01 +0100

    added proc, flags to microdocker

--- commands/microdocker.c
+++ commands/microdocker.c
@@ -1,17 +1,14 @@
+#define _GNU_SOURCE
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
+#include <sched.h>
+#include <sys/wait.h>
+#include <sys/mount.h>
 #include <string.h>
 
-int main(int argc, char **argv) {
-
-    if (argc < 3) return 1;
-
-    if (strcmp(argv[1], "run") == 0){
-        // for (int i = 0; i < argc; i++){
-        //     printf("%d=%s\n", i, argv[i]);
-        // }
-        
-        char* container_path = "alpineimage";
+int child(void* arg){
+    char* container_path = "alpineimage";
 
         if (chroot(container_path) != 0) {
             perror("chroot failed");
@@ -23,9 +20,15 @@ int main(int argc, char **argv) {
             return 1;
         }
 
+        if (mount("proc", "/proc", "proc", 0, NULL) != 0) {
+        perror("mount failed");
+        return 1;
+    }
+
+        char **args = (char **)arg;
         char command[256] = "";
-        for (int i = 2; i < argc; i++) {
-            strcat(command, argv[i]);
+        for (int i = 2; args[i] != NULL; i++) {
+            strcat(command, args[i]);
             strcat(command, " ");
         }
 
@@ -35,6 +38,32 @@ int main(int argc, char **argv) {
         execv("/bin/sh", new_argv);
 
         printf("execv failed");
+        return 1;
+}
+
+int main(int argc, char **argv) {
+
+    if (argc < 3) return 1;
+
+    if (strcmp(argv[1], "run") == 0){
+        
+        const int STACK_SIZE = 1024 * 1024;  // 1MB stack for the child
+        void *stack = malloc(STACK_SIZE);
+        if (!stack) {
+            perror("malloc failed");
+            return 1;
+        }
+
+        int flags = CLONE_NEWUTS | CLONE_NEWPID | SIGCHLD;
+        
+        pid_t pid = clone(child, stack + STACK_SIZE, flags, argv);
+        if (pid == -1) {
+            perror("clone failed");
+            free(stack);
+            return 1;
+        }
+        waitpid(pid, NULL, 0);
+        free(stack);
     }
     return 0;
 }
